261872dd6a2681348f98e4f1c2ace73762b396ad
Shohei Aoki <shoaok@gmail.com>
2016-09-14 08:38:10 +0000

    refactoring

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,11 +6,7 @@ class BoardsController < ApplicationController
   end
 
   def show
-    footprint_ids = [] 
-    @board.places.each do |p|
-      footprint_ids.push(p.footprint_id)
-    end
-    @footprints = Footprint.where(id: footprint_ids.uniq)
+    @footprints = Footprint.where(id: @board.places.pluck(:id).uniq)
   end
 
   def edit
